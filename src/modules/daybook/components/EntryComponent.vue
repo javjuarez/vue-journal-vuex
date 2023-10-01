f6244dfcc39caae43e4a0b3084f6d870aff09f08
Javier Juárez <sonico@ciencias.unam.mx>
2023-10-01 14:30:03 -0600

    Primer commit, punto de restauración

---
+++ src/modules/daybook/components/EntryComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        class="entry-container mb-3 pointer p-2"
+        @click="$router.push({name: 'entry', params: {id: 1}})"
+    >
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">15</span>
+            <span class="mx-1 fs-5">Julio</span>
+            <span class="mx-2 fw-light">2021,jueves</span>
+        </div>
+
+        <div class="entry-description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quisquam nobis eos perferendis doloribus quo sunt explicabo, illum voluptatum ex! Ex fugiat fugit eos quod, temporibus pariatur illo harum. Quae! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus facilis, dolor, odio iure repudiandae expedita asperiores corrupti nisi deserunt veritatis optio, consequuntur vero molestiae repellendus. Dicta sed a ea eveniet?
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+    }
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: "Commodo amet occaecat laborum anim in consectetur non esse id deserunt minim. Consectetur occaecat amet ad cupidatat. Esse Lorem elit adipisicing aute culpa magna. Laborum deserunt deserunt aliquip aute ad deserunt. Eu nostrud adipisicing ad incididunt commodo ut consectetur. Sint ipsum magna fugiat qui magna. Excepteur enim irure qui veniam aliqua consectetur.",
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: "Lorem eu eu sunt elit. Dolore quis do aliquip cillum esse aute eiusmod sint commodo in cillum. Et fugiat dolor esse nulla non eu pariatur cillum eu et magna. Et pariatur consectetur id et labore.",
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: "Ad cupidatat cupidatat ut tempor cupidatat anim sint consequat cupidatat fugiat. Commodo sit non ipsum laborum do dolor incididunt quis sit officia eiusmod culpa. Nulla et ut cillum aliquip et proident ad enim irure aliqua nisi minim excepteur. Commodo anim eiusmod nisi excepteur aliquip sint. In minim voluptate deserunt in pariatur sunt nulla.",
+            picture: null
+        },
+    ]
+})
